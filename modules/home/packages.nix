46b97cbc8d5145e01bb1a1bb1eae9a54b165bfd0
knightengel <knightoftheniche@gmail.com>
2026-03-12 16:41:15 +0300

    del all prog lan

--- modules/home/packages.nix
+++ modules/home/packages.nix
@@ -3,17 +3,14 @@
 {
   home.packages = with pkgs; [
     git
-    nodejs
     htop
     eza
     tree
     jq
-    rustup
     iina
     ani-cli
     zoxide
     qbittorrent
-    python314
     wezterm
     mpd
     rmpc
