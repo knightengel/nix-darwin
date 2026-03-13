3f1c3c32cd0c557cde9c0c733698e8114ef68d34
knightengel <knightoftheniche@gmail.com>
2026-03-13 17:01:57 +0300

    change lsp.lua

--- dotfiles/nvim/lua/plugins/lsp.lua
+++ dotfiles/nvim/lua/plugins/lsp.lua
@@ -1,40 +1,65 @@
 return {
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			vim.diagnostic.config({
-				virtual_text = true,
-				underline = true,
-				update_in_insert = false,
-				severity_sort = true,
-				float = {
-					border = "rounded",
-				},
-			})
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      vim.diagnostic.config({
+        virtual_text = true,
+        underline = true,
+        update_in_insert = false,
+        severity_sort = true,
+        float = {
+          border = "rounded",
+        },
+      })
 
-			vim.lsp.config("lua_ls", {
-				settings = {
-					Lua = {
-						diagnostics = {
-							globals = { "vim" },
-						},
-						workspace = {
-							checkThirdParty = false,
-						},
-					},
-				},
-			})
+      local lspconfig = require("lspconfig")
 
-			vim.lsp.config("nixd", {})
-			vim.lsp.config("nil_ls", {})
-			vim.lsp.config("pyright", {})
-      vim.lsp.config("rust_analyzer", {})
+      local on_attach = function(_, bufnr)
+        local opts = { buffer = bufnr, silent = true }
 
-			vim.lsp.enable("lua_ls")
-			vim.lsp.enable("nixd")
-			vim.lsp.enable("nil_ls")
-			vim.lsp.enable("pyright")
-      vim.lsp.enable("rust_analyzer")
-		end,
-	},
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>f", function()
+          vim.lsp.buf.format({ async = true })
+        end, opts)
+      end
+
+      lspconfig.lua_ls.setup({
+        on_attach = on_attach,
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              checkThirdParty = false,
+            },
+          },
+        },
+      })
+
+      lspconfig.nixd.setup({
+        on_attach = on_attach,
+      })
+
+      lspconfig.nil_ls.setup({
+        on_attach = on_attach,
+      })
+
+      lspconfig.pyright.setup({
+        on_attach = on_attach,
+      })
+
+      lspconfig.rust_analyzer.setup({
+        on_attach = on_attach,
+        autostart = true,
+      })
+    end,
+  },
 }
