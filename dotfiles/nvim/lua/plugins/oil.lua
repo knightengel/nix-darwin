7d7a4e7508a34bb6458ef24c95c4c4c39ed42b57
knightengel <knightoftheniche@gmail.com>
2026-03-14 10:16:51 +0300

    fix tree again

--- dotfiles/nvim/lua/plugins/oil.lua
+++ dotfiles/nvim/lua/plugins/oil.lua
@@ -5,11 +5,13 @@ return {
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
+    keys = {
+      { "<leader>e", "<cmd>Oil<CR>", desc = "Open explorer" },
+      { "-", "<cmd>Oil<CR>", desc = "Open parent directory" },
+    },
     opts = {
       default_file_explorer = true,
-      columns = {
-        "icon",
-      },
+      columns = { "icon" },
       view_options = {
         show_hidden = true,
       },
@@ -20,28 +22,5 @@ return {
         border = "rounded",
       },
     },
-    keys = {
-      { "-", "<cmd>Oil<CR>", desc = "Open parent directory" },
-      { "<leader>e", "<cmd>Oil<CR>", desc = "Open explorer" },
-    },
-    config = function(_, opts)
-      require("oil").setup(opts)
-
-      vim.api.nvim_create_autocmd("VimEnter", {
-        callback = function()
-          if vim.fn.argc() ~= 1 then
-            return
-          end
-
-          local arg = vim.fn.argv(0)
-          if vim.fn.isdirectory(arg) == 0 then
-            return
-          end
-
-          vim.cmd.cd(arg)
-          require("oil").open()
-        end,
-      })
-    end,
   },
 }
